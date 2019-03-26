ba355b0b26be8f11b3c7e24d4f0177e24e09a3da
Tony Sulfaro <tony116523@gmail.com>
2019-03-26 18:13:46 -0400

    finished examples

---
+++ Homework/hw16 - QT GUI Example/build-QT-Example3-Menu-NewWindow-Desktop_Qt_5_10_0_clang_64bit-Debug/ui_mydialog.h
@@ -0,0 +1,56 @@
+/********************************************************************************
+** Form generated from reading UI file 'mydialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.10.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MYDIALOG_H
+#define UI_MYDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QPushButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MyDialog
+{
+public:
+    QPushButton *CloseButton;
+
+    void setupUi(QDialog *MyDialog)
+    {
+        if (MyDialog->objectName().isEmpty())
+            MyDialog->setObjectName(QStringLiteral("MyDialog"));
+        MyDialog->resize(400, 300);
+        CloseButton = new QPushButton(MyDialog);
+        CloseButton->setObjectName(QStringLiteral("CloseButton"));
+        CloseButton->setGeometry(QRect(80, 120, 221, 91));
+
+        retranslateUi(MyDialog);
+        QObject::connect(CloseButton, SIGNAL(clicked()), MyDialog, SLOT(close()));
+
+        QMetaObject::connectSlotsByName(MyDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *MyDialog)
+    {
+        MyDialog->setWindowTitle(QApplication::translate("MyDialog", "Dialog", nullptr));
+        CloseButton->setText(QApplication::translate("MyDialog", "Close Window", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MyDialog: public Ui_MyDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MYDIALOG_H
